a583041e486b37484c2b9d9cd365cb846e42cd68
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-08-12 15:18:02 +0200

    handle zeros in _qr_fix

--- src/linear_algebra_ext.jl
+++ src/linear_algebra_ext.jl
@@ -16,7 +16,10 @@ end
 
 function _qr_fix(Q::T, R::AbstractMatrix) where {T <: AbstractMatrix}
     d = diag(R)
-    ph = d./abs.(d)
+    for i ∈ eachindex(d)
+        @inbounds d[i] = ifelse(isapprox(d[i], 0, atol=1e-14), 1, d[i])
+    end
+    ph = d ./ abs.(d)
     idim = size(R, 1)
     q = T.name.wrapper(Q)[:, 1:idim]
     return transpose(ph) .* q
@@ -25,9 +28,9 @@ end
 function LinearAlgebra.svd(A::AbstractMatrix, Dcut::Int, args...)
     U, Σ, V = psvd(A, rank=Dcut, args...)
     d = diag(U)
-    ph = d ./ abs.(d)
-    for i ∈ eachindex(ph)
-        @inbounds ph[i] = ifelse(isapprox(d[i], 0, atol=1e-14), 1, ph[i])
+    for i ∈ eachindex(d)
+        @inbounds d[i] = ifelse(isapprox(d[i], 0, atol=1e-14), 1, d[i])
     end
+    ph = d ./ abs.(d)
     return  U * Diagonal(ph), Σ, V * Diagonal(ph)
 end

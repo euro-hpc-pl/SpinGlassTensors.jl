a730e661f592f6ec646c2657e49fcbb0d204e497
Bartlomiej Gardas <bartek.gardas@gmail.com>
2021-08-12 17:37:12 +0200

    simplify logic (#8)
    * simplify logic
    
    * changed C[] to tr(C)

--- src/contractions.jl
+++ src/contractions.jl
@@ -12,11 +12,8 @@ export left_env, right_env, dot!
 function LinearAlgebra.dot(ϕ::AbstractMPS, ψ::AbstractMPS)
     T = promote_type(eltype(ψ), eltype(ϕ))
     C = ones(T, 1, 1)
-
-    for i ∈ eachindex(ψ)
-        M = ψ[i]
-        M̃ = conj(ϕ[i])
-        @tensor C[x, y] := M̃[β, σ, x] * C[β, α] * M[α, σ, y] order = (α, β, σ)
+    for (A, B) ∈ zip(ψ, ϕ)
+        @tensor C[x, y] := conj(B)[β, σ, x] * C[β, α] * A[α, σ, y] order = (α, β, σ)
     end
     tr(C)
 end
@@ -25,19 +22,15 @@ end
 Creates left environment
 """
 function left_env(ϕ::AbstractMPS, ψ::AbstractMPS)
-    l = length(ψ)
     T = promote_type(eltype(ψ), eltype(ϕ))
     S = typeof(similar(ψ[1], T, (1, 1)))
-    L = Vector{S}(undef, l+1)
+    L = Vector{S}(undef, length(ψ)+1)
     L[1] = similar(ψ[1], T, (1, 1))
     L[1][1, 1] = one(T)
 
-    for i ∈ 1:l
-        M = ψ[i]
-        M̃ = conj.(ϕ[i])
-
+    for (i, (A, B)) ∈ enumerate(zip(ψ, ϕ))
         C = L[i]
-        @tensor C[x, y] := M̃[β, σ, x] * C[β, α] * M[α, σ, y] order = (α, β, σ)
+        @tensor C[x, y] := conj(B)[β, σ, x] * C[β, α] * A[α, σ, y] order = (α, β, σ)
         L[i+1] = C
     end
     L
@@ -78,7 +71,6 @@ end
 
 @memoize Dict function right_env(ϕ::AbstractMPS{T}, W::AbstractMPO{T}, σ::Union{Vector, NTuple}) where {T}
     l = length(σ)
-    #k = length(ϕ)
     k = length(W)
     if l == 0
         R = similar(ϕ[1], T, (1, 1))
@@ -120,54 +112,39 @@ function LinearAlgebra.dot(ϕ::AbstractMPS, O::Union{Vector, NTuple}, ψ::Abstra
     C = similar(ψ[1], S, (1, 1))
     C[1, 1] = one(S)
 
-    for i ∈ eachindex(ψ)
-        M = ψ[i]
-        M̃ = conj.(ϕ[i])
-        Mat = O[i]
-        @tensor C[x, y] := M̃[β, σ, x] * Mat[σ, η] * C[β, α] * M[α, η, y] order = (α, η, β, σ)
+    for (A, W, B) ∈ zip(ϕ, O, ψ)
+        @tensor C[x, y] := conj(A)[β, σ, x] * W[σ, η] * C[β, α] * B[α, η, y] order = (α, η, β, σ)
     end
     tr(C)
 end
 
 
 function LinearAlgebra.dot(O::AbstractMPO, ψ::AbstractMPS)
-    L = length(ψ)
     S = promote_type(eltype(ψ), eltype(O))
     T = typeof(ψ)
-    ϕ = T.name.wrapper(S, L)
-
-    for i ∈ 1:L
-        W = O[i]
-        M = ψ[i]
-
-        @reduce N[(x, a), σ, (y, b)] := sum(η) W[x, σ, y, η] * M[a, η, b]
+    ϕ = T.name.wrapper(S, length(ψ))
+    for (i, (A, B)) ∈ enumerate(zip(O, ψ))
+        @reduce N[(x, a), σ, (y, b)] := sum(η) A[x, σ, y, η] * B[a, η, b]
         ϕ[i] = N
     end
     ϕ
 end
 
-function dot!(ψ::AbstractMPS, O::AbstractMPO)
-    L = length(ψ)
-    for i ∈ 1:L
-        W = O[i]
-        M = ψ[i]
 
-        @reduce N[(x, a), σ, (y, b)] := sum(η) W[x, σ, y, η] * M[a, η, b]
+function dot!(ψ::AbstractMPS, O::AbstractMPO)
+    for (i, (A, B)) ∈ enumerate(zip(ψ, O))
+        @reduce N[(x, a), σ, (y, b)] := sum(η) B[x, σ, y, η] * A[a, η, b]
         ψ[i] = N
     end
 end
 
+
 function LinearAlgebra.dot(O1::AbstractMPO, O2::AbstractMPO)
-    L = length(O1)
     S = promote_type(eltype(O1), eltype(O2))
     T = typeof(O1)
-    O = T.name.wrapper(S, L)
-
-    for i ∈ 1:L
-        W1 = O1[i]
-        W2 = O2[i]
-        @reduce V[(x, a), σ, (y, b), η] := sum(γ) W1[x, σ, y, γ] * W2[a, γ, b, η]
-
+    O = T.name.wrapper(S, length(O1))
+    for (i, (A, B)) ∈ enumerate(zip(O1, O2))
+        @reduce V[(x, a), σ, (y, b), η] := sum(γ) A[x, σ, y, γ] * B[a, γ, b, η]
         O[i] = V
     end
     O

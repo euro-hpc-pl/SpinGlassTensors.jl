b1ab553aff2abda87550706a1e860c34ab3357a9
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-09-13 01:17:58 +0200

    remove cuda tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,18 +10,6 @@ using Test
 
 idx(σ::Int) = (σ == -1) ? 1 : σ + 1
 
-my_tests = []
-# if CUDA.functional() && CUDA.has_cutensor() && false
-#     CUDA.allowscalar(false)
-#     include("cuda/test_helpers.jl")
-#     push!(
-#         my_tests,
-#         "cuda/base.jl",
-#         "cuda/contractions.jl",
-#         "cuda/compressions.jl",
-#         "cuda/spectrum.jl"
-#     )
-# end
 
 my_tests = [
     "base.jl",

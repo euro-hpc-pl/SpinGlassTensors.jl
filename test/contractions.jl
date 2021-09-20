87b801f6e528759eca614dc719aed579429d1771
bartekGardas <bartek.gardas@gmail.com>
2021-09-20 14:44:05 +0200

    clean up code

--- test/contractions.jl
+++ test/contractions.jl
@@ -34,10 +34,10 @@ Id_m = MPO(fill(ones(1,1,1,d), length(ϕ)))
     end
 
     @testset "renormalizations" begin
-        ψ[end] *= 1/norm(ψ)
+        ψ[end] *= 1 / norm(ψ)
         @test dot(ψ, ψ) ≈ 1
 
-        ϕ[1] *= 1/norm(ϕ)
+        ϕ[1] *= 1 / norm(ϕ)
         @test dot(ϕ, ϕ) ≈ 1
     end
 
@@ -52,17 +52,19 @@ Id_m = MPO(fill(ones(1,1,1,d), length(ϕ)))
 
 end
 
-@testset "left environment" begin
+@testset "left environment returns correct overlap" begin
     L = left_env(ϕ, ψ)
     @test L[end][1] ≈ dot(ϕ, ψ)
 end
 
-@testset "right environment" begin
+
+@testset "right environment returns correct overlap" begin
     R = right_env(ϕ, ψ)
     @test R[1][end] ≈ dot(ϕ, ψ)
 end
 
-@testset "Cauchy-Schwarz inequality of MPS" begin
+
+@testset "Cauchy-Schwarz inequality of MPS is OK" begin
     @test abs(dot(ϕ, ψ)) <= norm(ϕ) * norm(ψ)
 end
 
@@ -81,6 +83,7 @@ end
     
     @test tr(C) ≈ left_env(ψ, map(idx, state))[]
 end
+
 
 @testset "right_env correctly contracts MPO with MPS for a given configuration" begin
     D = 10

e608ba1649313600f6e073cec6bb6fbe3d9297eb
bartekGardas <bartek.gardas@gmail.com>
2021-09-29 14:55:29 +0200

    rm nasty functions

--- src/s_contractions.jl
+++ src/s_contractions.jl
@@ -12,21 +12,3 @@ function LinearAlgebra.dot(ψ::Mps, ϕ::Mps)
 end
 
 LinearAlgebra.norm(ψ::Mps) = sqrt(abs(dot(ψ, ψ)))
-
-
-# Jezu - K%$urwa max
-function LinearAlgebra.dot(O::Mpo, ϕ::Mps, j::Int)
-    D = Dict()
-    for i ∈ ϕ.sites
-        A = O.tensors[i]
-        B = ϕ.tensors[i]
-        G1 = A[j-4//6]
-        V = A[j-1//2]
-        T = A[j]
-        G2 = A[j+1//6]
-        @tensor C[l, x, r, p] := G1[x, y] * V[y, z] * T[l, z, r, d] * G2[d, p] order = (y, z, d)
-        @matmul E[(l, x), u, (r, y)] := sum(d) C[l, u, r, d] * B[x, d, y]
-        push!(D, i => E)
-    end
-    Mps(D)
-end

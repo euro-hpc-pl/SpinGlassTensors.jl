9ec2cba0b61f8cdbf19c3945ad565436ee528545
bartekGardas <bartek.gardas@gmail.com>
2021-09-29 17:03:00 +0200

    correct dot

--- src/s_contractions.jl
+++ src/s_contractions.jl
@@ -1,14 +1,15 @@
 export dot, truncate!
 
+
 function LinearAlgebra.dot(ψ::Mps, ϕ::Mps)
     T = promote_type(eltype(ψ.tensors[1]), eltype(ϕ.tensors[1]))
     C = ones(T, 1, 1)
 
-    for i ∈ ψ.sites
     for (i, (A, B)) ∈ enumerate(zip(ψ, ϕ))
         @tensor C[x, y] := conj(B)[β, σ, x] * C[β, α] * A[α, σ, y] order = (α, β, σ)
     end
     tr(C)
 end
+
 
 LinearAlgebra.norm(ψ::Mps) = sqrt(abs(dot(ψ, ψ)))

8cd439081b15730f9d7dd2fad5892a1bcad1398f
tomsmierz <smierzchalski.tomek@gmail.com>
2021-09-30 12:52:55 +0100

    Added types for functions

--- src/s_contractions.jl
+++ src/s_contractions.jl
@@ -15,23 +15,34 @@ end
 LinearAlgebra.norm(ψ::Mps) = sqrt(abs(dot(ψ, ψ)))
 
 function LinearAlgebra.dot(ψ::Mpo, ϕ::Mps)
+
 end
 
 
 function LinearAlgebra.dot(ψ::AbstractMpo, ϕ::Mps)
 end
 
-function contract_left(A,B)
+function contract_left(A::AbstractMatrix{T}, B::AbstractArray{T,3}) where {T}
     @cast C[(x,y), u, r] := sum(σ) A[x,σ] * B[(σ,y),u,r] (σ ∈ 1:size(B,2))
     C
 end
 
-function contract_up_small(A,B)
-    @tensor C[x,y,z] := A[y,σ] * B[x, σ, z] 
+function contract_up(A::AbstractMatrix{T}, B::AbstractArray{T,3}) where {T}
+    @tensor C[l,u,r] := A[u,σ] * B[l, σ, r] 
     C
 end
 
-function contract_up_big(A,B)
-    @cast C[(x,y),z,(b, a)] := sum(σ) A[y,z,a,σ] * B[x,σ,b]
+function contract_up(A::AbstractArray{T,3}, B::AbstractArray{T,4}) where {T}
+    @cast C[(x,y),z,(b, a)] := sum(σ) B[y,z,a,σ] * A[x,σ,b] 
+    C
+end
+
+function contract_up(A::AbstractMatrix{T}, B::AbstractArray{T,4}) where {T}
+    @tensor C[l,u,r,d] := A[u,σ] * B[l, σ, r, d] 
+    C
+end
+
+function contract_down(A::AbstractMatrix{T}, B::AbstractArray{T,4}) where {T}
+    @tensor C[l,u,r,d] := B[l,u,r,σ] * A[σ, d]
     C
 end

4f8f1bbaccbdce516677e66b4c7f8518cefc2165
Marek Rams <marek.rams@uj.edu.pl>
2021-12-08 13:17:40 +0100

    add IdentityQMPS

--- src/s_base.jl
+++ src/s_base.jl
@@ -1,4 +1,4 @@
-export QMps, QMpo, local_dims, SparseSiteTensor, SparseVirtualTensor
+export QMps, QMpo, local_dims, SparseSiteTensor, SparseVirtualTensor, IdentityQMps
 
 abstract type AbstractEnvironment end
 abstract type AbstractSparseTensor end
@@ -27,23 +27,53 @@ struct QMpo <: AbstractTensorNetwork{Number}
     QMpo(tensors::Dict) = new(tensors, sort(collect(keys(tensors))))
 end
 
-local_dim(mpo::QMpo, site::Site, dir::Symbol) = local_dim(mpo, site, Val(dir))
-function local_dim(mpo::QMpo, site::Site, ::Val{:up})
-    mkeys = sort(collect(keys(mpo[site])))
-    size(mpo[site][first(mkeys)], 2)
-end
+# local_dim(mpo::QMpo, site::Site, dir::Symbol) = local_dim(mpo, site, Val(dir))
+# function local_dim(mpo::QMpo, site::Site, ::Val{:up})
+#     mkeys = sort(collect(keys(mpo[site])))
+#     size(mpo[site][first(mkeys)], 2)
+# end
 
-function local_dim(mpo::QMpo, site::Site, ::Val{:down})
-    mkeys = sort(collect(keys(mpo[site])))
-    size(mpo[site][last(mkeys)], 4)
-end
+# function local_dim(mpo::QMpo, site::Site, ::Val{:down})
+#     mkeys = sort(collect(keys(mpo[site])))
+#     size(mpo[site][last(mkeys)], 4)
+# end
 
 function local_dims(mpo::QMpo, dir::Symbol)
     @assert dir ∈ (:down, :up)
-    Dict(site => local_dim(mpo, site, dir) for site ∈ mpo.sites)
+    lds = Dict()
+    for site ∈ mpo.sites
+        mkeys = sort(collect(keys(mpo[site])))
+        if any(length(size(mpo[site][kk])) > 2 for kk ∈ mkeys)
+            if dir == :down
+                ss = size(mpo[site][last(mkeys)])
+                ld = length(ss) == 4 ? ss[4] : ss[2]
+            else
+                ss = size(mpo[site][first(mkeys)])
+                ld = length(ss) == 4 ? ss[2] : ss[1]
+            end
+            push!(lds, site => ld)
+        end
+    end
+    lds
 end
 
-@inline Base.size(tens::AbstractSparseTensor, ind::Int) = maximum(tens.projs[ind])
+
+function IdentityQMps(loc_dims, Dmax::Int=1)
+    sites = sort(collect(keys(loc_dims)))
+    id = Dict()
+    for i ∈ 1:length(sites)
+        push!(id, sites[i] => zeros(Dmax, loc_dims[sites[i]], Dmax))
+    end
+    id[sites[begin]] = zeros(1, loc_dims[sites[begin]], Dmax)
+    id[sites[end]] = zeros(Dmax, loc_dims[sites[end]], 1)
+    for i ∈ 1:length(sites)
+        id[sites[i]][1, :, 1] .= 1 / sqrt(loc_dims[sites[i]])
+    end
+    QMps(id)
+end
+
+
+@inline Base.size(tens::AbstractSparseTensor) = collect(maximum(pr) for pr ∈ tens.projs)
 @inline function Base.setindex!(
     ket::AbstractTensorNetwork, A::AbstractArray, i::Site
 )

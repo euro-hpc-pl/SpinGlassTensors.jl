4a9d689d41ba0c0c9aa899350072360838b5d282
Bartek Gardas <bartek.gardas@gmail.com>
2022-07-15 16:29:15 +0200

    add custom kernel to add and project

--- src/s_compressions.jl
+++ src/s_compressions.jl
@@ -272,6 +272,31 @@ function update_env_left(
 end
 =#
 
+function __add_project!(ret, L, pr)
+    idx = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    x_stride = gridDim().x * blockDim().x
+
+    idy = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+    y_stride = gridDim().y * blockDim().y
+
+    for i ∈ idx:x_stride:size(L, 1), j ∈ idy:y_stride:size(L, 2)
+        for (σ, p) ∈ enumerate(pr)
+            @inbounds ret[i, j, p] += L[i, j, σ]
+        end
+    end
+end
+
+function add_project(L, pr, th=(16, 16))
+    n, m, _ = size(L)
+    bl = cld.((n, m), th)
+
+    proj = CUDA.CuArray(pr)
+    ret = CUDA.zeros(eltype(L), n, m, maximum(pr))
+
+    CUDA.@sync @cuda threads=th blocks=bl __add_project!(ret, L, proj)
+    ret
+end
+
 """
 $(TYPEDSIGNATURES)
 """
@@ -308,8 +333,12 @@ function update_env_left(
     Ln = Lnew_no_le .* reshape(loc_exp12, 1, 1, :)
     @reduce Lnew[x, y, σ] := sum(z) Ln[x, y, (z, σ)] (σ ∈ 1:size(pr, 1))
 
+@time begin
     ipr = CUDA.CuArray(diagm(ones(maximum(pr)))[pr, :])
     @tensor ret[x, y, r] := Lnew[x, y, z] * ipr[z, r]
+end
+
+    #@time ret = add_project(Lnew, pr)
 
     Array(permutedims(ret, (1, 3, 2)) ./ maximum(abs.(ret)))
 end

a0827daa320c20257f16d053cf24ce83db8709c3
Bartek Gardas <bartek.gardas@gmail.com>
2022-08-02 16:09:36 +0200

    modify sparse example

--- benchmark/sparse_example.jl
+++ benchmark/sparse_example.jl
@@ -2,6 +2,8 @@
 using CUDA
 using CUDA.CUSPARSE
 using SparseArrays
+using TensorOperations
+using TensorCast
 
 T = Float64
 n = 1000
@@ -9,7 +11,18 @@ n = 1000
 a = rand(T, n, n); a[a .< 0.9] .= T(0)
 a = CuSparseMatrixCSC(sparse(a))
 
-b = CUDA.rand(T, n, n);
+b = CUDA.rand(T, n, n)
+bb = CUDA.rand(T, n, n, n)
+
+#=
+@time begin
+    @tensor c[x, y, z] := b[x, y, s] * a[s, z]
+end
+
+@time begin
+    @matmul c[x, y, z] := sum(s) bb[x, y, s] * a[s, z]
+end
+=#
 
 @time a * b
 nothing

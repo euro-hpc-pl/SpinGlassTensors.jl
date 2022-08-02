918eb3f8e79dcb0cb188c9798d6f6df13941cd59
Bartek Gardas <bartek.gardas@gmail.com>
2022-08-02 16:24:23 +0200

    clean up sparse example

--- benchmark/sparse_example.jl
+++ benchmark/sparse_example.jl
@@ -6,23 +6,24 @@ using TensorOperations
 using TensorCast
 
 T = Float64
-n = 1000
+n = 100
 
-a = rand(T, n, n); a[a .< 0.9] .= T(0)
-a = CuSparseMatrixCSC(sparse(a))
+a = rand(T, n, n); a[a .< T(9//10)] .= T(0)
+a_d = CuSparseMatrixCSC(sparse(a))
 
 b = CUDA.rand(T, n, n)
-bb = CUDA.rand(T, n, n, n)
 
 #=
+bb = CUDA.rand(T, n, n, n)
+
 @time begin
-    @tensor c[x, y, z] := b[x, y, s] * a[s, z]
+    @tensor c[x, y, z] := b[x, y, s] * a_d[s, z]
 end
 
 @time begin
-    @matmul c[x, y, z] := sum(s) bb[x, y, s] * a[s, z]
+    @matmul c[x, y, z] := sum(s) bb[x, y, s] * a_d[s, z]
 end
 =#
 
-@time a * b
+@time a_d * b
 nothing

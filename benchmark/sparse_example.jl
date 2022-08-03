44540066840df5fcf377866415cb0f14964ab3b9
tomsmierz <smierzchalski.tomek@gmail.com>
2022-08-03 18:33:58 +0200

    sparse matrix benchmarking

--- benchmark/sparse_example.jl
+++ benchmark/sparse_example.jl
@@ -11,7 +11,11 @@ n = 100
 a = rand(T, n, n); a[a .< T(9//10)] .= T(0)
 a_d = CuSparseMatrixCSC(sparse(a))
 
-b = CUDA.rand(T, n, n)
+N = 1000
+
+pr = sort(rand(1:N, N^2))
+
+b = CUDA.rand(T, N^2, n)
 
 #=
 bb = CUDA.rand(T, n, n, n)
@@ -23,7 +27,34 @@ end
 @time begin
     @matmul c[x, y, z] := sum(s) bb[x, y, s] * a_d[s, z]
 end
-=#
 
-@time a_d * b
+@time  a_d * b 
 nothing
+
+@time begin
+    a = spzeros(maximum(pr), size(pr, 1))
+
+    for (index, i) in enumerate(pr)
+            a[i, index] = 1
+        end
+        a = CuSparseMatrixCSC(a)
+
+        a*b 
+    nothing
+end
+=#
+@time begin
+    CUDA.memory_status()
+    csrRowPtr = CuArray(collect(1:length(pr) + 1))
+    CUDA.memory_status()
+    csrColInd = CuArray(pr)
+    CUDA.memory_status()
+    csrNzVal = CUDA.ones(Float64, length(pr))
+    CUDA.memory_status()
+    a = CUSPARSE.CuSparseMatrixCSR(csrRowPtr, csrColInd, csrNzVal, (maximum(pr), length(pr)))
+    CUDA.memory_status()
+
+    a*b
+    CUDA.memory_status()
+    nothing
+end

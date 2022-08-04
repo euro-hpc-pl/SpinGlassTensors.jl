f55e7e6a9ac45361c263fe79bf6dab7d6e63e4bd
tomsmierz <smierzchalski.tomek@gmail.com>
2022-08-04 16:33:12 +0200

    test if a*b == b*a

--- benchmark/sparse_example.jl
+++ benchmark/sparse_example.jl
@@ -6,16 +6,18 @@ using TensorOperations
 using TensorCast
 
 T = Float64
-n = 100
+n = 1
+N = 3
 
 a = rand(T, n, n); a[a .< T(9//10)] .= T(0)
 a_d = CuSparseMatrixCSC(sparse(a))
 
-N = 1000
+
 
 pr = sort(rand(1:N, N^2))
 
-b = CUDA.rand(T, N^2, n)
+b = CUDA.rand(T, N^2, n, n)
+c = deepcopy(b)
 
 #=
 bb = CUDA.rand(T, n, n, n)
@@ -44,17 +46,36 @@ nothing
 end
 =#
 @time begin
-    CUDA.memory_status()
-    csrRowPtr = CuArray(collect(1:length(pr) + 1))
-    CUDA.memory_status()
-    csrColInd = CuArray(pr)
-    CUDA.memory_status()
-    csrNzVal = CUDA.ones(Float64, length(pr))
-    CUDA.memory_status()
-    a = CUSPARSE.CuSparseMatrixCSR(csrRowPtr, csrColInd, csrNzVal, (maximum(pr), length(pr)))
-    CUDA.memory_status()
+    @assert all(b .== c)
 
-    a*b
-    CUDA.memory_status()
-    nothing
+    #CUDA.memory_status()
+    csrRowPtr = CuArray(collect(1:length(pr) + 1))
+    #CUDA.memory_status()
+    csrColInd = CuArray(pr)
+    #CUDA.memory_status()
+    csrNzVal = CUDA.ones(Float64, length(pr))
+    #CUDA.memory_status()
+    a = CUSPARSE.CuSparseMatrixCSR(csrRowPtr, csrColInd, csrNzVal, (maximum(pr), length(pr)))
+    a2 = CUSPARSE.CuSparseMatrixCSR(csrRowPtr, csrColInd, csrNzVal, (length(pr), maximum(pr)))
+    
+    #CUDA.memory_status()
+    
+    #@cast c[x, (y, z)] := c[x, y, z]
+    c = reshape(c, (N^2, :))
+    d = a * c #(N, N^2) * (N^2, n^2)  =(N, n^2) 
+    #d = reshape(d, (N, n, n))
+    
+    b = permutedims(b, (3, 2, 1)) #(n, n, N^2)
+    #CUDA.memory_status()
+    #@cast b[(x, y), z] := b[x, y, z] #(n^2, N^2)
+    b = reshape(b, (:, N^2))
+   
+    e = b * a2 #(n^2, N^2) * (N^2, N) = (n^2, N)
+    e = permutedims(e, (2,1))
+    #@assert isequal(d,e)
+    #e = reshape(e, (N, n, n))
+    
+    println(d)
+    println(e)
+
 end

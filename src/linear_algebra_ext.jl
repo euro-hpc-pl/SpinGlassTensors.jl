8622c79688fa27b4eb63d421ac00b7e114d7f146
bartekGardas <bartek.gardas@gmail.com>
2022-12-18 12:18:45 +0100

    clean up linear alg ext

--- src/linear_algebra_ext.jl
+++ src/linear_algebra_ext.jl
@@ -2,47 +2,34 @@ export
     rq_fact,
     qr_fact
 
-function qr_fact(M::AbstractMatrix, Dcut::Int=typemax(Int), tol::Float64=1E-16, args...)
-    q, r = _qr_fix(qr(M, args...))
-    if Dcut >= size(q, 2) return q, r end
+@inline function phase(d::T; atol::T=eps()) where T <: Real
+    isapprox(d, zero(T), atol=atol) ? one(T) : d / abs(d)
+end
+
+function qr_fact(M::AbstractMatrix{T}, Dcut::Int=typemax(Int), tol::T=eps(), args...) where T <: Real
+    q, r = qr_fix(qr(M, args...))
+    Dcut >= size(q, 2) && return q, r
     U, Σ, V = svd(r, Dcut, tol)
     q * U, Σ .* V'
 end
 
-function rq_fact(M::AbstractMatrix, Dcut::Int=typemax(Int), tol::Float64=1E-15, args...)
+function rq_fact(M::AbstractMatrix{T}, Dcut::Int=typemax(Int), tol::T=eps(), args...) where T <: Real
     q, r = qr_fact(M', Dcut, tol, args...)
     r', q'
 end
 
-function _qr_fix(QR_fact::T) where T <: LinearAlgebra.QRCompactWY
-    d = diag(QR_fact.R)
-    L = length(d)
-    ph = zeros(L, L)
-    for i ∈ 1:L
-        @inbounds ph[i, i] = ifelse(
-            isapprox(d[i], 0, atol=1e-14), 1, d[i] / abs(d[i])
-        )
-    end
-    QR_fact.Q * ph, diag(ph) .* QR_fact.R
+function qr_fix(QR_fact::LinearAlgebra.QRCompactWY; tol::T=eps()) where T <: Real
+    ϕ = phase.(diag(QR_fact.R); atol=tol)
+    QR_fact.Q * Diagonal(ϕ), ϕ .* QR_fact.R
 end
 
-function LinearAlgebra.svd(
-    A::AbstractMatrix, Dcut::Int=typemax(Int), tol::Float64=1E-15, args...
-)
+function LinearAlgebra.svd(A::AbstractMatrix{T}, Dcut::Int=typemax(Int), tol::T=eps(), args...) where T <: Real
     U, Σ, V = svd(A, args...)
 
-    tol = Σ[begin] * max(eps(), tol)
-    δ = min(Dcut, sum(Σ .> tol))
+    δ = min(Dcut, sum(Σ .> Σ[1] * max(eps(), tol)))
+    U, Σ, V = U[:, 1:δ], Σ[1:δ], V[:, 1:δ]
+    Σ ./= sum(Σ .^ 2)
 
-    U = U[:, begin:δ]
-    Σ = Σ[begin:δ]
-    #Σ ./= sum(Σ .^ 2)
-    V = V[:, begin:δ]
-
-    d = diag(U)
-    for i ∈ eachindex(d)
-        @inbounds d[i] = ifelse(isapprox(d[i], 0, atol=1e-14), 1, d[i])
-    end
-    ph = d ./ abs.(d)
-    U * Diagonal(ph), Σ, V * Diagonal(ph)
+    ϕ = Diagonal(phase.(diag(U); atol=tol))
+    U * ϕ, Σ, V * ϕ
 end

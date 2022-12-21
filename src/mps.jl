4f86e84d3438d4949c1fbc9570c43a8dcc48e130
bartekGardas <bartek.gardas@gmail.com>
2022-12-21 18:43:02 +0100

    make MpoTensor external const

--- src/mps.jl
+++ src/mps.jl
@@ -30,38 +30,35 @@ struct MpoTensor{T <: Real, N}
     ctr:: Union{Tensor{T, N}, Nothing}
     bot::Vector{Tensor{T, 2}}  # N == 2 bot = []
     dims::Dims{N}
-
-    function MpoTensor{T, N}(top, ctr, bot, dims) where {T, N}
-        new{T, N}(top, ctr, bot, dims)
-    end
-
-    function MpoTensor(ten::TensorMap{T}) where T
-        sk = sort(collect(keys(ten)))
-        top = [ten[k] for k ∈ sk if k < 0]
-        bot = [ten[k] for k ∈ sk if k > 0]
-        ctr = get(ten, 0, nothing)
-
-        if isnothing(ctr)
-            top_bot = vcat(top, bot)
-            dims = (0, size(top_bot[1], 1), 0, size(top_bot[end], 2))
-            nn = 4
-        else
-            nn = ndims(ctr)
-            if nn == 2
-                @assert isempty(top) && isempty(bot) "Both top and bot should be empty"
-                dims = size(ctr)
-            elseif nn == 4
-                dims = (
-                    size(ctr, 1), isempty(top) ? size(ctr, 2) : size(top[1], 1),
-                    size(ctr, 3), isempty(bot) ? size(ctr, 4) : size(bot[end], 2)
-                )
-            else
-                throw(DomainError(ndims(ctr), "MpoTensor should have ndims 2 or 4"))
-            end
-        end
-        new{T, nn}(top, ctr, bot, dims)
-    end
 end
+
+function MpoTensor(ten::TensorMap{T}) where T
+    sk = sort(collect(keys(ten)))
+    top = [ten[k] for k ∈ sk if k < 0]
+    bot = [ten[k] for k ∈ sk if k > 0]
+    ctr = get(ten, 0, nothing)
+
+    if isnothing(ctr)
+        top_bot = vcat(top, bot)
+        dims = (0, size(top_bot[1], 1), 0, size(top_bot[end], 2))
+        nn = 4
+    else
+        nn = ndims(ctr)
+        if nn == 2
+            @assert isempty(top) && isempty(bot) "Both top and bot should be empty"
+            dims = size(ctr)
+        elseif nn == 4
+            dims = (
+                size(ctr, 1), isempty(top) ? size(ctr, 2) : size(top[1], 1),
+                size(ctr, 3), isempty(bot) ? size(ctr, 4) : size(bot[end], 2)
+            )
+        else
+            throw(DomainError(ndims(ctr), "MpoTensor should have ndims 2 or 4"))
+        end
+    end
+    MpoTensor{T, nn}(top, ctr, bot, dims)
+end
+
 
 Base.ndims(ten::MpoTensor{T, N}) where {T, N} = N
 Base.size(ten::MpoTensor, n::Int) = ten.dims[n]

a2ff69a110db830a01c96f8a3b2015f4b4e3fe80
marekrams <marek.rams@uj.edu.pl>
2022-12-22 13:07:06 +0100

    zipper

--- src/variational.jl
+++ src/variational.jl
@@ -1,6 +1,7 @@
 export
     variational_compress!,
-    variational_sweep!
+    variational_sweep!,
+    zipper
 
 function variational_compress!(
     bra::QMps{T}, mpo::QMpo{T}, ket::QMps{T}, tol::Real=1E-10, max_sweeps::Int=4, args...
@@ -63,4 +64,41 @@ function variational_sweep!(   # we may be able to remove it
 ) where T <: Real
     env = Environment(bra, mpo, ket)
     _left_sweep_var!(env, args...)
+end
+
+function zipper(ψ::QMpo{R}, ϕ::QMps{R}, Dcut::Int=typemax(Int), tol::Real=eps(), args...) where R <: Real
+    # input ϕ should be canonized :left
+    # results should be canonized :right
+    D = TensorMap{R}()
+    T = ones(R, 1, 1, 1)
+    mpo_li = last(ψ.sites)
+    for i ∈ reverse(ϕ.sites)
+        while mpo_li > i
+            T = contract_matrix_tensor3(ψ[mpo_li], T)
+            mpo_li = left_nbrs_site(mpo_li, ψ.sites)
+        end
+        @assert mpo_li == i "mismatch between QMpo and QMps sites."
+        mpo_li = left_nbrs_site(mpo_li, ψ.sites)
+
+        M, B = ψ[i], ϕ[i]
+        for v ∈ reverse(M.bot) B = contract_matrix_tensor3(v, B) end
+
+        @tensor TT[l, ml, mt, tt] := M.ctr[ml, mt, mr, mb] * B[l, mb, r] * T[tt, mr, r] order = (r, mb, mr)
+        s1, s2, s3, s4 = size(TT)
+        @cast TT[(t1, t2), t3, t4] := TT[t1, t2, t3, t4]
+        for v ∈ reverse(M.top) TT = contract_matrix_tensor3(v, TT) end
+        @cast TT[t12, (t3, t4)] := TT[t12, t3, t4]
+
+        U, Σ, V = svd(TT, Dcut, tol, args...)
+        V = Array(V')
+        T = U * diagm(Σ)
+        if i == first(ϕ.sites)
+            V = T * V
+        end
+        @cast V[x, y, z] := V[x, (y, z)] (y ∈ 1:s3)
+        @cast T[x, y, z] := T[(x, y), z] (y ∈ 1:s2)
+        T = permutedims(T, (3, 2, 1))
+        push!(D, i => V)
+    end
+    QMps(D)
 end

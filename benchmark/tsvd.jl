17f619b7336fe3a635a018844a469def6bdebf97
bartekGardas <bartek.gardas@gmail.com>
2022-12-22 14:30:15 +0100

    improve bench

--- benchmark/tsvd.jl
+++ benchmark/tsvd.jl
@@ -1,33 +1,65 @@
-using TSVD
+using LinearAlgebra, MKL
 using TensorOperations
 using TensorCast
-using LinearAlgebra
-
+using TSVD
+using LowRankApprox
+using RandomizedLinAlg
 
 # C = A * B
-struct MyTensor{T <: Real}
+struct MyTensor{T <: Number}
     A::Array{T, 2}
     B::Array{T, 2}
 end
 
+Base.Array(ten::MyTensor) = ten.A * ten.B
+
+# this is for tsvd to work
 Base.eltype(ten::MyTensor{T}) where T = T
-Base.size(ten::MyTensor{T}) where T = (size(ten.A, 1), size(ten.B, 2))
-Base.size(ten::MyTensor{T}, n::Int) where T = size(ten)[n]
+Base.size(ten::MyTensor) = (size(ten.A, 1), size(ten.B, 2))
+Base.size(ten::MyTensor, n::Int) = size(ten)[n]
 Base.adjoint(ten::MyTensor{T}) where T = MyTensor{T}(adjoint(ten.B), adjoint(ten.A))
 Base.:(*)(ten::MyTensor{T}, v::Vector{T}) where T = (ten.A * (ten.B * v))
 
-n = 100
-cut = 10
+# this is for psvd to work
+LinearAlgebra.ishermitian(ten::MyTensor) = ishermitian(ten.A) && ishermitian(ten.B)
+LinearAlgebra.mul!(y, ten::MyTensor, x) = mul!(y, ten.B, ten.A * x)
+
+n = 2 ^ 12
+cut = 2 ^ 6
 T = Float64
 
 ten = MyTensor(rand(T, n, n), rand(T, n, n))
+
+println("MyTensor:")
+println("tsvd:")
 @time U, Σ, V = tsvd(ten, cut)
 
-# basic example
-#=
-A = rand(T, n, n)
+println("psvd:")
+@time U, Σ, V = psvd(ten, rank=cut)
 
-@time U, Σ, V = tsvd(A, cut)
-=#
+println("Array:")
+println("tsvd:")
+@time begin
+    C = Array(ten)
+    U, Σ, V = tsvd(C, cut)
+end
+
+println("svd:")
+@time begin
+    C = Array(ten)
+    U, Σ, V = svd(C)
+end
+
+println("psvd:")
+@time begin
+    C = Array(ten)
+    U, Σ, V = psvd(C, rank=cut)
+end
+
+println("rsvd:")
+@time begin
+    C = Array(ten)
+    U, Σ, V = rsvd(C, cut, 0)
+end
 
 nothing

5e099868dcb8bd7414db130b36c5b8bf76caf963
marekrams <marek.rams@uj.edu.pl>
2022-12-23 17:31:02 +0100

    zipper psvd

--- src/zipper.jl
+++ src/zipper.jl
@@ -13,6 +13,17 @@ struct CornerTensor{T <: Real}
     end
 end
 
+struct ACornerTensor{T <: Real}
+    C::Tensor{T, 3}
+    M::MpoTensor{T, 4}
+    B::Tensor{T, 3}
+
+    function ACornerTensor(ten)
+        T = eltype(ten)
+        new{T}(ten.C, ten.M, ten.B)
+    end
+end
+
 function zipper(ψ::QMpo{R}, ϕ::QMps{R}, method::Symbol=:svd; Dcut::Int=typemax(Int), tol::Real=eps(), args...) where R <: Real
     # input ϕ should be canonized :left
     # results should be canonized :right
@@ -27,7 +38,8 @@ function zipper(ψ::QMpo{R}, ϕ::QMps{R}, method::Symbol=:svd; Dcut::Int=typemax
         @assert mpo_li == i "Mismatch between QMpo and QMps sites."
         mpo_li = left_nbrs_site(mpo_li, ψ.sites)
 
-        U, Σ, V = svd_corner_matrix(Val(method), C, ψ[i], ϕ[i], Dcut, tol, args...) # TODO ctr here?
+        CM = CornerTensor(C, ψ[i], ϕ[i])
+        U, Σ, V = svd_corner_matrix(Val(method), CM, Dcut, tol, args...)
 
         C = U * diagm(Σ)
         s1 = size(ψ[i], 1)
@@ -42,9 +54,8 @@ function zipper(ψ::QMpo{R}, ϕ::QMps{R}, method::Symbol=:svd; Dcut::Int=typemax
     QMps(D)
 end
 
-function corner_matrix(
-    C::S, M::T, B::S
-) where {S <: CuArrayOrArray{R, 3}, T <: MpoTensor{R, 4}} where R <: Real
+function Base.Array(CM::CornerTensor)
+    B, M, C = CM.B, CM.M, CM.C 
     for v ∈ reverse(M.bot) B = contract_matrix_tensor3(v, B) end
     Cnew = corner_matrix(C, M.ctr, B)
     @cast Cnew[(t1, t2), t3, t4] := Cnew[t1, t2, t3, t4]
@@ -53,19 +64,16 @@ function corner_matrix(
     Cnew
 end
 
-function svd_corner_matrix(::Val{:svd}, C, M, B, Dcut, tol, args...)
-    svd(corner_matrix(C, M, B), Dcut, tol, args...)
+function svd_corner_matrix(::Val{:svd}, CM, Dcut, tol, args...)
+    svd(Array(CM), Dcut, tol, args...)
 end
 
-function svd_corner_matrix(::Val{:psvd}, C, M, B, Dcut, tol, args...)
-    psvd(corner_matrix(C, M, B), rank=Dcut)
+function svd_corner_matrix(::Val{:psvd}, CM, Dcut, tol, args...)
+    psvd(Array(CM), rank=Dcut)
 end
 
-function svd_corner_matrix(::Val{:psvd_sparse}, C, M, B, Dcut, tol, args...)
-    ct = CornerTensor(C, M, B)
-    println(size(ct))
-    println(eltype(ct))
-    psvd(CornerTensor(C, M, B), rank=Dcut)
+function svd_corner_matrix(::Val{:psvd_sparse}, CM, Dcut, tol, args...)
+    psvd(CM, rank=Dcut)
 end
 
 # this is for psvd to work
@@ -73,7 +81,19 @@ LinearAlgebra.ishermitian(ten::CornerTensor) = false
 Base.size(ten::CornerTensor) = (size(ten.B, 1) * size(ten.M, 1), size(ten.C, 1) * size(ten.M, 2))
 Base.size(ten::CornerTensor, n::Int) = size(ten)[n]
 Base.eltype(ten::CornerTensor{T}) where T = T
+
+Base.adjoint(ten::CornerTensor{T}) where T = ACornerTensor(ten)
+
 function LinearAlgebra.mul!(y, ten::CornerTensor, x)
-    x = reshape(x, 1, size(ten.M, 2), size(ten.C, 1))
-    y[:] = reshape(update_env_right(ten.C, x, ten.M, ten.B), :)
+    x = reshape(x, size(ten.M, 2), size(ten.C, 1), :)
+    x = permutedims(x, (3, 1, 2))
+    yp = update_env_right(ten.C, x, ten.M, ten.B)
+    y[:] = reshape(permutedims(yp, (3, 2, 1)), :)
+end
+
+function LinearAlgebra.mul!(y, ten::ACornerTensor, x)
+    x = reshape(x, size(ten.B, 1), size(ten.M, 1), :)
+    x = Array(x)
+    yp = project_ket_on_bra(x, ten.B, ten.M, ten.C)
+    y[:] = reshape(yp, :)
 end

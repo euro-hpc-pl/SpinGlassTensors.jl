edfa3bb0e2dc833cdc9641bd1d730b01f1529fbb
bartekGardas <bartek.gardas@gmail.com>
2022-12-28 18:47:07 +0100

    fix array 2

--- src/base.jl
+++ src/base.jl
@@ -15,7 +15,7 @@ const CuArrayOrArray{T, N} = Union{AbstractArray{T, N}, CuArray{T, N}}
 # Allow data to reside on CUDA ???
 
 move_to_CUDA!(ten::Array{T, N}) where {T, N} = CuArray(ten)
-move_to_CUDA!(ten::Diagonal) = CuArry(Diagonal(CuArray(diag(ten))))
+move_to_CUDA!(ten::Diagonal) = CuArray(Diagonal(CuArray(diag(ten))))
 
 
 device(ten::AbstractArray) = Set(typeof(ten) <: CuArray)

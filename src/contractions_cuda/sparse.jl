431e21c4169b14330913e7a001c3c6a536fb14fc
marekrams <marek.rams@uj.edu.pl>
2023-01-25 09:19:10 +0100

    aux_saprse

--- src/contractions_cuda/sparse.jl
+++ src/contractions_cuda/sparse.jl
@@ -1,8 +1,14 @@
 # @memoize Dict
+
+@memoize Dict function aux_cusparse(::Type{R}, n::Int64) where R <: Real
+    CuArray(1:n+1), CUDA.ones(R, n)
+end
+
 function CUDA.CUSPARSE.CuSparseMatrixCSC(::Type{R}, p::CuArray{Int64, 1}) where R <: Real
     n = length(p)
     mp = maximum(p)
-    CuSparseMatrixCSC(CuArray(1:n+1), p, CUDA.ones(R, n), (mp, n))
+    cn, co = aux_cusparse(R, n)
+    CuSparseMatrixCSC(cn, p, co, (mp, n))
 end
 
 function CUDA.CUSPARSE.CuSparseMatrixCSC(::Type{T}, p1::R, p2::R, p3::R) where {T <: Real, R <: CuArray{Int64, 1}}

1c1aa8d106b603ce93cd878395bb235a802b82ed
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2023-02-19 14:27:50 +0100

    test measure_spectrum

--- test/canonise.jl
+++ test/canonise.jl
@@ -35,3 +35,14 @@ id = Dict(j => d[i] for (i, j) in enumerate(sites))
         end
     end
 end
+
+@testset "Measure spectrum" begin
+    svd_mps = TensorMap{T}(
+        1 => [1.0;;;0.0], 3//2 => [1.0 0.0;;; 1.0 0.0], 
+        2 => permutedims([1.0 0.0; 1.0 0.0 ;;;], (1,3,2))
+    )
+    ψ = QMps(svd_mps) 
+    canonise!(ψ, :left)
+    A = measure_spectrum(ψ)
+    @test [A[i]==[1.0] for i in keys(A)] == Bool[1, 1, 1]
+end

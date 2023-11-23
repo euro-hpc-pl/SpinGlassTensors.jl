efccf1c95e201c13e906be73e7da171078260137
annamariadziubyna <73058800+annamariadziubyna@users.noreply.github.com>
2023-11-23 19:28:05 +0100

    add docs

--- src/mps/base.jl
+++ src/mps/base.jl
@@ -13,6 +13,22 @@ const Site = Union{Int, Rational{Int}}
 const Sites = NTuple{N, Site} where N
 const TensorMap{T} = Dict{Site, Union{Tensor{T, 2}, Tensor{T, 3}, Tensor{T, 4}}}  # 2 and 4 - mpo;  3 - mps
 
+"""
+A mutable struct representing a Matrix Product Operator (MPO) tensor in a tensor network.
+
+## Fields
+- `top::Vector{Tensor{T, 2}}`: Vector of tensors representing the top tensor of the MPO. Empty if `N == 2`.
+- `ctr::Union{Tensor{T, N}, Nothing}`: Central tensor of the MPO. `Nothing` if not present.
+- `bot::Vector{Tensor{T, 2}}`: Vector of tensors representing the bottom tensor of the MPO. Empty if `N == 2`.
+- `dims::Dims{N}`: Dimensions of the MPO tensor.
+
+## Description
+`MpoTensor{T, N}` is a mutable struct that represents a Matrix Product Operator tensor in a tensor network.     
+The MPO tensor is characterized by its top and bottom tensors, a central tensor (`ctr`), and dimensions (`dims`). 
+The top and bottom legs are vectors of two-dimensional tensors (`Tensor{T, 2}`). 
+The central tensor is of type `Tensor{T, N}` or `Nothing` if not present. 
+The dimensions of the MPO tensor are specified by `dims`. 
+"""
 mutable struct MpoTensor{T <: Real, N}
     top::Vector{Tensor{T, 2}}  # N == 2 top = []
     ctr::Union{Tensor{T, N}, Nothing}
@@ -20,6 +36,26 @@ mutable struct MpoTensor{T <: Real, N}
     dims::Dims{N}
 end
 
+# """
+# Constructor function for creating a Matrix Product Operator (MPO) tensor from a `TensorMap`.
+
+#  ## Arguments
+# - `ten::TensorMap{T}`: A dictionary mapping `Site` indices to tensors of type `Tensor{T, 2}`, `Tensor{T, 3}`, or `Tensor{T, 4}`. The key `0` represents the central tensor (`ctr`) of the MPO.
+    
+# ## Returns
+# - An instance of `MpoTensor{T, nn}` representing the Matrix Product Operator.
+    
+# ## Description
+# The `MpoTensor` function constructs a Matrix Product Operator tensor from a `TensorMap`, 
+# where the keys are `Site` indices and the values are tensors of appropriate dimensions. 
+# The construction process involves sorting the tensor dictionary based on the site indices 
+# and separating tensors into the top, central, and bottom parts. 
+# The central tensor is identified by the key `0`. 
+# The resulting `MpoTensor` encapsulates the tensors along with their dimensions.
+        
+# ## Exceptions
+# - Throws a `DomainError` if the central tensor (`ctr`) has dimensions other than 2 or 4.
+# """ 
 function MpoTensor(ten::TensorMap{T}) where T
     sk = sort(collect(keys(ten)))
     top = [ten[k] for k ∈ sk if k < 0]

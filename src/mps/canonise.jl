1dcbdf3757dd2de7513c51f421d62d1a4a9c191f
Łukasz Pawela <lukasz.pawela@gmail.com>
2024-04-03 23:41:42 +0200

    initial approach to remove cast

--- src/mps/canonise.jl
+++ src/mps/canonise.jl
@@ -12,7 +12,8 @@ function measure_spectrum(ψ::QMps{T}) where {T<:Real}
     for i ∈ reverse(ψ.sites)
         B = permutedims(Array(ψ[i]), (1, 3, 2)) # [x, σ, α]
         @matmul M[x, σ, y] := sum(α) B[x, σ, α] * R[α, y]
-        @cast M[x, (σ, y)] := M[x, σ, y]
+        # @cast M[x, (σ, y)] := M[x, σ, y] TODO: restore when deps merged
+        M = reshape(M, :, size(M, 2) * size(M, 3))
         Dcut, tolS = 100000, 0.0
         U, S, _ = svd_fact(Array(M), Dcut, tolS)
         push!(schmidt, i => S)
@@ -71,10 +72,12 @@ function _right_sweep!(
         A = ψ[i]
         @matmul M[x, y, σ] := sum(α) R[x, α] * A[α, y, σ]
         M = permutedims(M, (3, 1, 2))  # [σ, x, y]
-        @cast M[(σ, x), y] := M[σ, x, y]
+        # @cast M[(σ, x), y] := M[σ, x, y] TODO: restore when deps merged
+        M = reshape(M, size(M, 1) * size(M, 2), :)
         Q, R = qr_fact(M, Dcut, tolS; toGPU = ψ.onGPU, kwargs...)
         R ./= maximum(abs.(R))
-        @cast A[σ, x, y] := Q[(σ, x), y] (σ ∈ 1:size(A, 3))
+        # @cast A[σ, x, y] := Q[(σ, x), y] (σ ∈ 1:size(A, 3)) TODO: restore when deps merged
+        A = reshape(Q, size(A, 3), size(Q, 1) ÷ size(A, 3), size(Q, 2))
         ψ[i] = permutedims(A, (2, 3, 1))  # [x, y, σ]
     end
 end
@@ -89,10 +92,13 @@ function _left_sweep!(
     for i ∈ reverse(ψ.sites)
         B = permutedims(ψ[i], (1, 3, 2)) # [x, σ, α]
         @matmul M[x, σ, y] := sum(α) B[x, σ, α] * R[α, y]
-        @cast M[x, (σ, y)] := M[x, σ, y]
+        # @cast M[x, (σ, y)] := M[x, σ, y]
+        M = reshape(M, size(M, 1), size(M, 2) * size(M, 3))
         R, Q = rq_fact(M, Dcut, tolS; toGPU = ψ.onGPU, kwargs...)
         R ./= maximum(abs.(R))
-        @cast B[x, σ, y] := Q[x, (σ, y)] (σ ∈ 1:size(B, 2))
+        # @cast B[x, σ, y] := Q[x, (σ, y)] (σ ∈ 1:size(B, 2))
+        B = reshape(Q, size(Q, 1),  size(B, 2), size(Q, 2) ÷ size(B, 2))
+
         ψ[i] = permutedims(B, (1, 3, 2))
     end
 end

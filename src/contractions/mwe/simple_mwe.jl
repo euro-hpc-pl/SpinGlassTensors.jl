f12af511e2f07eae7e1d3c331aedc5dfddd1948b
Bartek <bartek.gardas@gmail.com>
2025-01-04 13:29:02 +0000

    clean up

--- src/contractions/mwe/simple_mwe.jl
+++ src/contractions/mwe/simple_mwe.jl
@@ -11,10 +11,7 @@ B = rand(T, 1, 1)
 C = rand(T, 2, 2, 2, 1)
 
 if onGPU
-    A1 = CuArray(A1)
-    A2 = CuArray(A2)
-    B = CuArray(B)
-    C = CuArray(C)
+    A1, A2, B, C = CuArray.((A1, A2, B, C))
 end
 
 mul!(A1, B, (@view C[1, 2, :, :])')
